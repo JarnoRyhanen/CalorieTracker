d0598617c09c86e933864cd10ce89c921f509dfe
Jarno02 <jarno.ryhanen02@gmail.com>
2022-08-24 11:30:59 +0300

    Imported project

--- app/src/main/java/com/plcoding/calorytracker/ui/theme/Shape.kt
+++ app/src/main/java/com/plcoding/calorytracker/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.home.calorietracker.ui.theme
+package com.plcoding.calorytracker.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
